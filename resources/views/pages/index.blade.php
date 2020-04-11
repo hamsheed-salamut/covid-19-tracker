0add7db9b56f1656fb1ffcbe497ce6308cc868ea
Hamsheed <Hamsheed@HAMSHEED>
2020-04-11 16:06:50 +0400

    Changed label

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -63,11 +63,11 @@
           </div>
         </div>
         <div class="row">
-          <div class="col-md-8 grid-margin stretch-card">
+          <div class="col-md-12 grid-margin stretch-card">
             <div class="card">
               <div class="card-body">
                 <div class="clearfix">
-                  <h4 class="card-title float-left">Visit And Sales Statistics</h4>
+                  <h4 class="card-title float-left">Worldwide Statistics</h4>
                   <div id="visit-sale-chart-legend"
                     class="rounded-legend legend-horizontal legend-top-right float-right"></div>
                 </div>
